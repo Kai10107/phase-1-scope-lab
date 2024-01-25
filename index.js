e5f65f709ae1d6a5456746b40a67cb02ec6e17aa
kai10107 <kaiaustin@icloud.com>
2024-01-25 13:21:09 +1100

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 1
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = '1'; 
+}
